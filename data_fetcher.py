160138ee9a554b46fcbf4bd8b024ba973b414a63
pator-pixel <pator@gmx.net>
2026-06-07 14:51:49 +0200

    env und gitignore hinzugefügt

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,16 +1,24 @@
+import os
 import requests
+from dotenv import load_dotenv
 
-API_KEY = "lXLWfFAmhsHw47RLE6EINEUA4PNN0wrhv8K9y6gO"
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
 
 def fetch_data(animal_name):
     url = "https://api.api-ninjas.com/v1/animals"
-    headers = {"X-Api-Key": API_KEY}
-    params = {"name": animal_name}
+
+    headers = {
+        "X-Api-Key": API_KEY
+    }
+
+    params = {
+        "name": animal_name
+    }
 
     response = requests.get(url, headers=headers, params=params)
-
-    print(response.status_code)
-    print(response.json())
 
     if response.status_code == 200:
         return response.json()
